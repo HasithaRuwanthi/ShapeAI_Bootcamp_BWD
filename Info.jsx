fbc0cbbe42f2fcb28b843f6ef8ce0224c5d3d4d7
Hasitha Ruwanthi <hasitharuwanthi568@gmail.com>
2021-06-27 07:27:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1 >Javascript and React.js</h1>
+      <p >This is a great Bootcamp.I was able to learn React.js clearly and simply.Thnk you very much sir!...</p>
+    </div>
+  );
+}
+export default Info;
